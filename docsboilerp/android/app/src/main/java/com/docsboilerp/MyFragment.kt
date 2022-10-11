92025051b9277089ac5ffc91aafa657c3589bc6d
aravindwme <Aravind.d@warrantyme.co>
2022-10-11 17:06:02 +0530

    type mismatch context

--- docsboilerp/android/app/src/main/java/com/docsboilerp/MyFragment.kt
+++ docsboilerp/android/app/src/main/java/com/docsboilerp/MyFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.facebook.react.uimanager.ThemedReactContext
 
 // replace with your view's import
 import com.docsboilerp.CustomView
